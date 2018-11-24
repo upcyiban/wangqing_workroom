6287162c6d0e5974e31002365b0865e1d167e7ce
zxshane <3031178263@qq.com>
2018-11-24 20:23:51 +0800

    all

---
+++ src/components/homepage.vue
@@ -0,0 +1,239 @@
+<template>
+  <div class="homepage">
+    <router-link to="questionList" ><div class="question" v-show="isteacher">
+      问题列表<img style="transform:translateY(5px);" src="../assets/questionlist.png" width="25px" height="25px"/>
+    </div></router-link>
+    <div>
+       <div class="photo first">
+          <img :src="teacher[2].head_img" width="80px" height="80px" >
+       </div>
+       <div class="nameshuo first">
+         {{ teacher[2].teacher_name }}老师
+       </div>
+       <div class="photo second">
+          <img :src="teacher[3].head_img" width="80px" height="80px" >
+       </div>
+       <div class="nameshuo second">
+         {{ teacher[3].teacher_name }}老师
+       </div>
+        <div class="photo third">
+          <img :src="teacher[4].head_img" width="80px" height="80px" >
+       </div>
+       <div class="nameshuo third">
+         {{ teacher[4].teacher_name }}老师
+       </div>
+        <div class="photo fourth">
+          <img :src="teacher[5].head_img" width="80px" height="80px" >
+       </div>
+       <div class="nameshuo fourth">
+         {{ teacher[5].teacher_name }}老师
+       </div>
+        <div class="photo fiveth">
+          <img :src="teacher[6].head_img" width="80px" height="80px" >
+       </div>
+       <div class="nameshuo fiveth">
+         {{ teacher[6].teacher_name }}老师
+       </div>
+       <div class="photo sixth">
+          <img :src="teacher[7].head_img" width="80px" height="80px" >
+       </div>
+       <div class="nameshuo sixth">
+         {{ teacher[7].teacher_name }}老师
+       </div>
+        <div class="photo seventh">
+          <img :src="teacher[8].head_img" width="80px" height="80px" >
+       </div>
+       <div class="nameshuo seventh">
+         {{ teacher[8].teacher_name }}老师
+       </div>
+    </div>
+      <router-link to="question"><div class="ask">
+        <img id="askimg" src="../assets/question.png" width="80px" height="80px">
+       <p style="transform:translateY(-8px);"> 我要提问</p>
+      </div></router-link>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Answer",
+  data() {
+    return {
+      islike:false,
+      info:[],
+      teacher:[],
+      identity:'',
+      isteacher:false
+    };
+  },
+  created(){
+    this.getinfo()
+    this.getteacher()
+    //this.getOneAnswer()
+  },
+  methods:{
+    getinfo:function(){
+    let location=window.location.href
+    let data=location.split(/[?=&]/)
+    this.vq=data[2]
+    console.log(this.vq)
+    this.$axios({
+      url:'http://yb.upc.edu.cn:8086/info/?vq='+this.vq
+    }).then(response=>{
+      console.log(response.data)
+      window.localStorage.setItem('student_name',response.data.yb_username)
+      window.localStorage.setItem('head_img',response.data.yb_userhead)
+      window.localStorage.setItem('identity',response.data.yb_identity)
+      window.localStorage.setItem('yb_id',response.data.yb_userid)
+   
+       this.identity=window.localStorage.getItem('identity')
+        if(this.identity=="老师")
+        {
+          this.isteacher=true
+          console.log(this.isteacher)
+        }
+
+    })
+    if(window.localStorage.getItem('islike')==undefined)
+       window.localStorage.setItem('islike',false)
+    },
+    dianzan:function(){
+      this.islike=!this.islike
+    },
+    getteacher:function(){
+      this.$axios({
+        url:"http://yb.upc.edu.cn:8086/teacher_head_img/"
+      }).then(response=>{
+        this.teacher = response.data
+      })
+    },
+    getOneAnswer:function(){
+      this.$axios({
+        url:'http://yb.upc.edu.cn:8086/answer/'
+      }).then(response=>{
+        console.log(response.data)
+        if(response.data!=undefined)
+        {
+          this.$router.push('/answer')
+        }
+      })
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a{
+  text-decoration: none;
+  color: black;
+}
+.question{
+  position: absolute;
+  margin-left:35%;
+  margin-top:140%;
+  background-color: white;
+  border-radius: 25px;
+  padding-bottom:1%;
+  padding-left:2%;
+  padding-right:2%;
+  color: black;
+
+}
+.photo {
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  border-color: white;
+  border-style: solid;
+  background-color: white;
+  position: absolute;
+  z-index: 888;
+  }
+.nameshuo{
+  background-color:white;
+  opacity: 0.7;
+  width: 80px;
+  border-radius: 25px;
+  position: absolute;
+  z-index: 999;
+  color: black;
+  margin-top:70px;
+  text-align: center;
+  font-size: 12px;
+  font-weight: 700;
+  padding:1%;
+
+}
+.first{
+  margin-left: 12%;
+  transform: translateY(110px);
+}
+.second{
+  margin-left:65%;
+  transform: translateY(80px);
+}
+.third{
+  margin-left: 40%;
+  transform: translateY(100px);
+}
+.fourth{
+  margin-left:5%;
+  transform: translateY(250px);
+}
+.fiveth{
+  margin-left:70%;
+ transform: translateY(240px);
+}
+.sixth{
+  margin-left:25%;
+  transform: translateY(400px);
+}
+.seventh{
+  margin-left: 60%;
+  transform: translateY(350px);
+}
+.ask{
+  position: absolute;
+  width: 140px;
+  height: 140px;
+  margin-top:280px;
+  background-color: white;
+  border-radius: 70px;
+  z-index: 999;
+  margin-left: 29%;
+  text-align: center;
+  transform: translateY(-50px);
+  color: black;
+
+}
+img {
+  border-radius: 50px;
+}
+#askimg{
+   transform: translateY(15px)
+}
+
+.homepage {
+  background-image: url(../assets/background.jpg);
+  background-size: cover;
+  top:0;
+  height: 720px;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,29 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Question from '@/components/Question'
 import QuestionList from '@/components/QuestionList'
+import Answer from '@/components/answer'
+import Homepage from '@/components/homepage'
+import Auth from'@/components/yibanauth'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'Auth',
+      component: Auth
+    },
+    {
+      path:'/answer',
+      name:'Answer',
+      component: Answer
+    },
+    {
+      path:'/homepage',
+      name:'HomePage',
+      component:Homepage
+    },
     {
       path: '/question',
       name: 'Question',
